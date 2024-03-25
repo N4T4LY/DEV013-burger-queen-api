74b4656bb0ef4130181c580f19a02521080a7b34
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-03-24 22:45:12 -0400

    feat(controller/products.js): adding postProducts function to create a new product

--- controller/products.js
+++ controller/products.js
@@ -1,4 +1,5 @@
 const { connect } = require('../connect.js');
+const {ObjectId}=require("mongodb")
 module.exports = {
   //see products
   getProducts: async (req, res, next) => {
@@ -20,46 +21,50 @@ module.exports = {
 
   //create product
 
+postProducts: async (req, res) => {
+  try {
+    const { name, price, image, type } = req.body;
+    if (!name || !price || !image || !type) {
+      return res.status(400).json({ error: "Todos los campos son requeridos" });
+    }
 
+    if (typeof price !== "number" || price <= 0) {
+      return res
+        .status(400)
+        .json({ error: "El precio debe ser un número positivo" });
+    }
 
+    
+    const db = await connect();
+    const collection = db.collection("product");
+    const existingProduct = await collection.findOne({name:name});
+    if(existingProduct){
+      return res.status(400).json({error: "El producto ya existe en la BD"})
+    }
+    
+    const date=new Date();
+    const currentDate=date.toLocaleString()
+
+    const result = await collection.insertOne({
+      name: name,
+      price: price,
+      image: image,
+      type: type,
+      dateEntry: currentDate,
+    });
+    res.json({result, msg: "se creo correctamente" });
+  } catch (error) {
+    console.error("Error ", error);
+    res.status(500).json({ error: "Error en el servidor " });
+  }
+},
 
 
 };
 
 
 
-// //find product
-// getProductById: async (req, res) => {
-//   try {
-//     const db = await connect();
-//     if (!db) {
-//       return res
-//         .status(500)
-//         .json({ error: "Error de conexión a la base de datos" });
-//     }
-//     const collection = db.collection("product");
-//     if (!collection) {
-//       return res
-//         .status(500)
-//         .json({ error: "La colección de productos no existe" });
-//     }
-//     const { productId } = req.params;
-//     if (!ObjectId.isValid(productId)) {
-//       return res.status(400).json({ error: "ID de producto inválido" });
-//     }
-//     const cursor = await collection
-//       .find({ _id: new ObjectId(productId) })
-//       .toArray();
-//     if (cursor.length === 0) {
-//       return res.status(404).json({ error: "Producto no encontrado" });
-//     }
-//     console.log("🚀 ~ app.get ~ cursor:", cursor);
-//     res.json(cursor);
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res.status(500).json({ error: "Error en el servidor" });
-//   }
-// };
+
 
 
 // put("/products/:productId", async (req, res) => {
