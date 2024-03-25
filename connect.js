f8004aedc2069d75a04efac5f0568aea205a0e79
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-03-24 20:59:30 -0400

    feat(connect.js): add BD conexion of bqDatabase

--- connect.js
+++ connect.js
@@ -1,10 +1,22 @@
 const config = require('./config');
+//importo MongoClient 
+const {MongoClient}=require('mongodb');
 
 // eslint-disable-next-line no-unused-vars
 const { dbUrl } = config;
-
+//creo el cliente 
+const client = new MongoClient(dbUrl)
 async function connect() {
   // TODO: Database Connection
+  try {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db("bqDatabase");
+    return db;
+  }catch(error){
+    console.log("🚀 ~ connect ~ error:", error)
+    
+  }
 }
 
 module.exports = { connect };
