d721dde923665cf7acf79c5816b0c1938f93f968
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-04-04 11:42:48 -0400

    feat(routes/users.js): Check if the user exists and if not, create it

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ const {
 const { Collection } = require('mongodb');
 const { connect } = require('../connect');
 
-const initAdminUser = (app, next) => {
+const initAdminUser = async (app, next) => {
   const { adminEmail, adminPassword } = app.get('config');
   if (!adminEmail || !adminPassword) {
     return next();
@@ -20,10 +20,24 @@ const initAdminUser = (app, next) => {
   const adminUser = {
     email: adminEmail,
     password: bcrypt.hashSync(adminPassword, 10),
-    roles: "admin",
+    role: "admin",
   };
 
+  const db= await connect()
+    const collection = db.collection("user");
+    const existingAdmin= await collection.findOne({
+    email:adminUser.email,
+    })
+    console.log(adminUser.email)
+
+    if(!existingAdmin){
+      const insertAdmin = await collection.insertOne(adminUser);
+      console.log(adminUser.email)
+    }
+
+
   // TODO: Create admin user
+  
   // First, check if adminUser already exists in the database
   // If it doesn't exist, it needs to be saved
 
