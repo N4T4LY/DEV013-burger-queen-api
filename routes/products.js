529bac9d19aee95bfdee032551b8a733bec417c0
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-04-04 18:16:21 -0400

    feat(routes/products.js): Adding product routes and authentication middleware in module.exports

--- routes/products.js
+++ routes/products.js
@@ -1,24 +1,12 @@
-const {
-  requireAuth,
-  requireAdmin,
-} = require('../middleware/auth');
+const { getProducts, postProducts, getProductById, putProductById, deleteProduct } = require("../controller/products");
+const { requireAuth, requireAdmin } = require("../middleware/auth");
 
 module.exports = (app, nextMain) => {
-
-  app.get('/products', requireAuth, (req, resp, next) => {
-  });
-
-  app.get('/products/:productId', requireAuth, (req, resp, next) => {
-  });
-
-  app.post('/products', requireAdmin, (req, resp, next) => {
-  });
-
-  app.put('/products/:productId', requireAdmin, (req, resp, next) => {
-  });
-
-  app.delete('/products/:productId', requireAdmin, (req, resp, next) => {
-  });
+  app.get("/products", requireAuth, getProducts);
+  app.post("/products", requireAdmin, postProducts);
+  app.get("/products/:productId", requireAuth, getProductById);
+  app.put("/products/:productId", requireAdmin, putProductById);
+  app.delete("/products/:productId", requireAdmin, deleteProduct);
 
   nextMain();
 };
