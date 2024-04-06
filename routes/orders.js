476760049424401ed26f4967a1deef25b6985068
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-04-06 19:36:54 -0400

    feat(/controller/orders.js): creating routes of orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,22 +1,32 @@
+const { getOrders, postOrders, getOrderById, putOrderById, deleteOrder } = require("../controller/orders");
 const {
   requireAuth,
 } = require('../middleware/auth');
 
 module.exports = (app, nextMain) => {
-  app.get('/orders', requireAuth, (req, resp, next) => {
-  });
+  app.get("/orders", requireAuth, getOrders);
+  app.post("/orders", requireAuth, postOrders);
+  app.get("/orders/:orderId", requireAuth, getOrderById);
+  app.put("/orders/:orderId", requireAuth, putOrderById);
+  app.delete("/orders/:orderId", requireAuth, deleteOrder);
 
-  app.get('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
 
-  app.post('/orders', requireAuth, (req, resp, next) => {
-  });
 
-  app.put('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
 
-  app.delete('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
+  // app.get('/orders', requireAuth, (req, resp, next) => {
+  // });
+
+  // app.get('/orders/:orderId', requireAuth, (req, resp, next) => {
+  // });
+
+  // app.post('/orders', requireAuth, (req, resp, next) => {
+  // });
+
+  // app.put('/orders/:orderId', requireAuth, (req, resp, next) => {
+  // });
+
+  // app.delete('/orders/:orderId', requireAuth, (req, resp, next) => {
+  // });
 
   nextMain();
 };
